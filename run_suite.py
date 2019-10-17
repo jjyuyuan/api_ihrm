4779ba44d3e7318f6c0a426af3d693bc83d772ca
jjyuyuan <jjyuyuan@qq.com>
2019-10-17 15:28:36 +0800

    runner

--- run_suite.py
+++ run_suite.py
@@ -1,10 +1,15 @@
 import unittest
 
+import app
 from case.test_login import TestLogin
 from case.test_user import TestUser
+from tools.HTMLTestRunner import HTMLTestRunner
 
 suite = unittest.TestSuite()
 suite.addTest(TestLogin("test_login_success"))
 suite.addTest(unittest.makeSuite(TestUser))
-runner = unittest.TextTestRunner()
-runner.run(suite)
+# runner = unittest.TextTestRunner()
+# runner.run(suite)
+with open(app.PRO_PATH + "/report/report.html", "wb", encoding="utf-8") as f:
+    runner = HTMLTestRunner(f, title="ihrm_report", description="ihrm_report")
+    runner.run(suite)
